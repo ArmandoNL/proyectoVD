72c02be5c81377d9fb1abdaea18b1c6059bda90a
Armando Nevares <armandonl14@hotmail.com>
2016-11-07 10:38:43 -0600

    Inicial

---
+++ ProyectoVD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoVD.Global" Language="C#" %>
---
+++ ProyectoVD/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoVD.Startup))]
+namespace ProyectoVD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
